b3761d25d13719bb4ac4f4e906a517b96aabf7c2
Chi-Yun Hsu <chiyunhsu@users.noreply.github.com>
2025-06-16 16:54:33 -0700

    Add use of `have` tactic

--- SummerResearch2025/Demo.lean
+++ SummerResearch2025/Demo.lean
@@ -1,7 +1,7 @@
 import Mathlib
 
--- P is a quantifier, Q and R are propositions
-variable {α : Type*} (P : α → Prop) (Q R: Prop)
+-- P is a quantifier, Q, R and S are propositions
+variable {α : Type*} (P : α → Prop) (Q R S: Prop)
 
 -- Forward reasoning for Modus Ponens
 example (h : Q → R) (hQ : Q) : R := by
@@ -10,13 +10,30 @@ example (h : Q → R) (hQ : Q) : R := by
   exact h hQ
 -- Instead of using `exact`, we can also simply write `example (h : Q → R) (hQ : Q) : R := h hQ`, removing the `by` and `exact` to mean `h hQ` is the proof of the example.
 
---Backward reasoning for Modus Ponens
+-- Backward reasoning
+-- Modus Ponens
 example (h : Q → R) (hQ : Q) : R := by
 -- The `apply` tactic is used to apply a hypothesis or a theorem to the current goal, making the hypotheses the new goal.
   apply h
 -- The `exact` tactic is used when the goal is already an hypothesis or a theoroem.
   exact hQ
 
+-- Forward reasoning
+-- `have` is used to introduce an intermeidate step
+example (h1 : Q → R) (h2 : R → S)(hQ : Q) : S := by
+  have hR : R := by
+    sorry
+  have hS : S := by
+    sorry
+  exact hS
+
+-- Backward reasoning
+example (h1 : Q → R) (h2 : R → S)(hQ : Q) : S := by
+  apply h2
+  apply h1
+  exact hQ
+
+-- Quantifiers
 example (x : α) (h : ∀ x, P x) : P x := by
   exact h x
 
